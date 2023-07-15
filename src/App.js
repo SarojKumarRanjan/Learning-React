e390010b89c0152423836bde0b0a4eaa2f307c6d
SarojKumarRanjan <sarojranjan555@gmail.com>
2023-07-15 16:42:58 +0530

    Added conditional statement to show verification in the side of channel name

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ return(
 <div className="App">
 <h1 className="Heading-title">Videos</h1>
 
-  <Video title="Learning react tutorial 1" channel="Coder Dost" views=" 100k" time="7 months ago"></Video>
-  <Video title="Learning react tutorial 2" channel="Coder Dost" views=" 700k" time="6 months ago"></Video>
-  <Video title="Learning react tutorial 3" channel="Coder Dost"  views=" 600k" time="5 months ago"></Video>
-  <Video title="Learning react tutorial 4" channel="Coder Dost"  views=" 160k" time="4 months ago"></Video>
-  <Video title="Learning react tutorial 5" channel="Coder Dost"  views=" 170k" time="3 months ago"></Video>
-  <Video title="Learning react tutorial 6" channel="Coder Dost"  views=" 106k" time="2 months ago"></Video>
+  <Video verified={true} title="Learning react tutorial 1" channel="Coder Dost" views=" 100k" time="7 months ago"></Video>
+  <Video verified={true}title="Learning react tutorial 2" channel="Coder Dost" views=" 700k" time="6 months ago"></Video>
+  <Video verified={false} title="Learning react tutorial 3" channel="Coder Dost"  views=" 600k" time="5 months ago"></Video>
+  <Video verified={true} title="Learning react tutorial 4" channel="Coder Dost"  views=" 160k" time="4 months ago"></Video>
+  <Video verified={false} title="Learning react tutorial 5" channel="Coder Dost"  views=" 170k" time="3 months ago"></Video>
+  <Video verified={true} title="Learning react tutorial 6" channel="Coder Dost"  views=" 106k" time="2 months ago"></Video>
   
    </div>)
 
--- src/components/Video.js
+++ src/components/Video.js
@@ -2,18 +2,23 @@ import './Video.css';
 
 
 
-function Video ({title,time,views,channel}){
+function Video ({title,time,views,channel,verified}){
+
+
 return(
 
     <>
     <div className="container">
    <div className="pic">
-   <img src="https://picsum.photos/300/200" alt="Random-img" />
+   <img src="https://picsum.photos/id/78/300/200" alt="Random-img" />
    </div>
+
+  <div class="channel" >{channel} { verified && "verified"} </div>
+   
    
 
     <div class="dark" >{ title }</div>
-    <div class="channel" >{channel}</div>
+   
     <div class="views">
         {views} views <span>.</span> {time}
     </div>
